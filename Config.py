206b744524fc6523065de69895378dc34d9c7116
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-09-15 10:09:57 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -20,7 +20,7 @@ else:
     API_ID = 25742938
     API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
     BOT_TOKEN = "7288949255:AAGPUJpHg138ArQjnN_1ON4EULI4Jb3pGe0"
-    DATABASE_URL = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+    DATABASE_URL = "postgres://lwktrpsc:1UlTfWHhnxKZWRETlLu6AF0x4AsyKXhO@rosie.db.elephantsql.com/lwktrpsc"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "HEROKUBIN_01"
     if MUST_JOIN.startswith("@"):
